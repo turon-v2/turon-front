93a5a81d04a9806d16b1934bf3af180521b1a581
Baxtiyor <87475561+Baxtiyor1@users.noreply.github.com>
2022-11-03 16:04:14 +0500

    change media styles and add modal

--- src/Components/header/modal.jsx
+++ src/Components/header/modal.jsx
@@ -1,14 +1,30 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
-function Modal() {
+function Modal({active}) {
   return (
     <>
-    <section className="modal">
-
-    </section>
+      <section className={!active ? "modal" : "modal modal--active"}>
+        <ul className="modal__list">
+          <li className="modal__item">
+            <Link href="/" className="modal__link">Bosh sahifa</Link>
+          </li>
+          <li className="modal__item">
+            <Link href="/product" className="modal__link">Mahsulotlar</Link>
+          </li>
+          <li className="modal__item">
+            <Link href="/recipe" className="modal__link">Retseplar</Link>
+          </li>
+          <li className="modal__item">
+            <Link href="/about" className="modal__link">Haqida</Link>
+          </li>
+          <li className="modal__item">
+            <Link href="/contact" className="modal__link">Aloqa</Link>
+          </li>
+        </ul>
+      </section>
     </>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
