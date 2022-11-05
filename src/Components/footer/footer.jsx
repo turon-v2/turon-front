82f5c26a5f7f279581f21a3dd385cc3c1cee0c02
Zohidbek <87475601+kholikulovz@users.noreply.github.com>
2022-11-05 11:20:32 +0500

    footer with responsive is ready

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -1,19 +1,66 @@
 import Image from "next/image"
 import Link from "next/link"
-// import useLang from '../../Hooks/useLang';
-// import language from '../../Assets/content';
 
 // import images
-
+import footerLogo from "../../Assets/images/footer-logo.svg"
+import Svg from "../../Assets/svg"
 
 
 
 function Footer() {
-
-  // let [lang, setlang] = useLang()
-  // const [selected, setSelected] = useState(lang);
   return (
-    <></>
+    <div className="footer__wrapper">
+      <div className="container">
+        <Svg />
+        <nav className="footer__nav">
+          <Link className="footer__logo-link" href={'/'}>
+            <Image className="footer__logo" src={footerLogo} alt="logo" width="239" height="89" />
+          </Link>
+          <ul className="footer__list">
+            <li className="footer__item">
+              <Link href="/" className="footer__link">Bosh sahifa</Link>
+            </li>
+            <li className="footer__item">
+              <Link href="/product" className="footer__link">Mahsulotlar</Link>
+            </li>
+            <li className="footer__item">
+              <Link href="/recipe" className="footer__link">Retseplar</Link>
+            </li>
+            <li className="footer__item">
+              <Link href="/about" className="footer__link">Haqida</Link>
+            </li>
+            <li className="footer__item">
+              <Link href="/contact" className="footer__link">Aloqa</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="footer__social">
+          <div className="footer__social-list">
+            <Link href="https://www.facebook.com/turonun/" target='_blank' rel='noreferrer'
+              className="footer__social-item">
+              <svg className="footer__social-img">
+                <use xlinkHref="#facebook"></use>
+              </svg>
+            </Link>
+            <Link href="https://www.instagram.com/turonun" target='_blank' rel='noreferrer'
+              className="footer__social-item">
+              <svg className="footer__social-img">
+                <use xlinkHref="#instagram"></use>
+              </svg>
+            </Link>
+            <Link href="https://t.me/turonun" target='_blank' rel='noreferrer'
+              className="footer__social-item">
+              <svg className="footer__social-img">
+                <use xlinkHref="#telegram"></use>
+              </svg>
+            </Link>
+          </div>
+        </div>
+        <div className="footer__bottom">
+          <p className="footer__bottom-text">©2022 Barcha huquqlar himoyalangan</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
