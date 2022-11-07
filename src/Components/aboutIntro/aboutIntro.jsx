a49aadc4e7c01171fdf380cc7802fae635fc0e97
Zohidbek <87475601+kholikulovz@users.noreply.github.com>
2022-11-07 10:28:07 +0500

    about page is ready

---
+++ src/Components/aboutIntro/aboutIntro.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import Link from "next/link"
+
+// import images
+import AboutTop from '../../Assets/images/about-top.png'
+
+
+function AboutIntro() {
+
+  return (
+    <>
+    <section className="aboutIntro">
+      <div className="container">
+        <div className="aboutIntro__wrapper">
+        <Image className="aboutIntro__img" src={AboutTop} alt="about top image" width="1280" height="720" />
+        </div>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default AboutIntro
