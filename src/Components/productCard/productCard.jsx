415a436091b8d15ce014044055dc493ceadeae65
Zohidbek <87475601+kholikulovz@users.noreply.github.com>
2022-11-25 19:47:18 +0500

    fixed new merge bugs

--- src/Components/productCard/productCard.jsx
+++ src/Components/productCard/productCard.jsx
@@ -1,9 +1,19 @@
+import Image from "next/image"
 
-export const ProductCard = ({children,  bgcolor: backgroundColor}) =>{
-console.log(children, backgroundColor);
+
+export const ProductCard = ({img, title, desc, children,  bgcolor: backgroundColor}) =>{
+// console.log(children, backgroundColor);
     return (
         <div className="productCard" style={{backgroundColor}}>
-            {children}
+            {children} 
+            {/* <div className="product__img--box">
+                <Image src={img} alt="turon img" className="product__img" />
+              </div>
+              <div className="product__box">
+                <h3 className="product__title">{title}</h3>
+                <p className="product__info">{desc}</p>
+                <LinkBtn bgcolor="#fff" children="Batafsil" href="/product/1"/> 
+              </div> */}
         </div>
     )
 }
--- src/Components/product_item/product_item.jsx
+++ src/Components/product_item/product_item.jsx
@@ -11,7 +11,7 @@ export const ProductItem = ({img, title, desc, bgcolor, color}) => {
               <div className="product__box">
                 <h3 className="product__title">{title}</h3>
                 <p className="product__info">{desc}</p>
-                <LinkBtn bgcolor="#fff" children="Batafsil" href="/product/1"/>
+                {/* <LinkBtn bgcolor="#fff" children="Batafsil" href="/product/1"/> */}
               </div>
             </li>
         </>
